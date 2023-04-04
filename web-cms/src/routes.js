30e912e14bcd2bd019534cd0b57342c62fe03af1
gilang <kadekgilangputra@gmail.com>
2023-04-05 07:53:24 +0800

    rename web admin -> cms

--- web-cms/src/routes.js
+++ web-cms/src/routes.js
@@ -51,8 +51,7 @@ const Toasts = React.lazy(() => import('./views/notifications/toasts/Toasts'))
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/', name: 'Dashboard', element: Dashboard, exact: true },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
   { path: '/theme/typography', name: 'Typography', element: Typography },
